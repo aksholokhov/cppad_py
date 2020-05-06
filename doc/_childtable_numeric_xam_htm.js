6d26ee97ab18992537f054e0d19f1a56074cee2d
Brad Bell <bradbell@seanet.com>
2020-05-06 13:06:51 -0700

    update gh-pages to version 2020.5.6

--- doc/_childtable_numeric_xam_htm.js
+++ doc/_childtable_numeric_xam_htm.js
@@ -2,14 +2,18 @@
 document.write('\
 <select onchange="numeric_xam_child(this)">\
 <option>numeric_xam-&gt;</option>\
-<option>numeric_optimize_xam.py</option>\
-<option>numeric_ode_runge_xam.py</option>\
+<option>numeric_runge4_one_step</option>\
+<option>numeric_runge4_multi_step</option>\
+<option>numeric_optimize_fun_class</option>\
+<option>numeric_seirs_class</option>\
 </select>\
 ');
 function numeric_xam_child(item)
 {	var child_list = [
-		'numeric_optimize_xam.py.htm',
-		'numeric_ode_runge_xam.py.htm'
+		'numeric_runge4_one_step.htm',
+		'numeric_runge4_multi_step.htm',
+		'numeric_optimize_fun_class.htm',
+		'numeric_seirs_class.htm'
 	];
 	var index = item.selectedIndex;
 	item.selectedIndex = 0;
--- doc/_search.htm.js
+++ doc/_search.htm.js
@@ -3,13 +3,19 @@
 // ------------------------------------------------------------ 
 Keyword = 
 [
-{ tag: 'cppad_py', title:'cppad_py-2020.5.5: A C++ Object Library and Python Interface to CppAD', other:' git repository purpose getting started numerical examples function speed license' },
+{ tag: 'cppad_py', title:'cppad_py-2020.5.6: A C++ Object Library and Python Interface to CppAD', other:' git repository purpose getting started numerical examples function speed license' },
 { tag: 'setup.py', title:'Configure and Build the cppad_py Python Module', other:' syntax external requirements install using pip errors download top source directory get test c++ import path' },
 { tag: 'install_error', title:'Error Messages During Installation', other:' solved swig permissions cppad library missing fortify source unsolved travis' },
 { tag: 'get_cppad.sh', title:'Get Cppad', other:' syntax top source directory settings cppad_prefix extra_cxx_flags build_type test_cppad caching' },
 { tag: 'numeric_xam', title:'Numerical Examples', other:'' },
-{ tag: 'numeric_optimize_xam.py', title:'Example Using Scipy trust-constr Optimization Method', other:' reference problem trust_constr source code' },
-{ tag: 'numeric_ode_runge_xam.py', title:'Example Computing Derivative A Runge-Kutta Ode Solution', other:' source code' },
+{ tag: 'numeric_runge4_one_step', title:'One Fourth Order Runge-Kutta ODE Step', other:' syntax ti yi yf example source code' },
+{ tag: 'numeric_runge4_multi_step', title:'Multiple Fourth Order Runge-Kutta ODE Steps', other:' syntax t_all y_init y_all example source code' },
+{ tag: 'numeric_runge4_one_step_xam.py', title:'Example Computing Derivative A Runge-Kutta Ode Solution', other:' source code' },
+{ tag: 'numeric_runge4_multi_step_xam.py', title:'Example Computing Derivative A Runge-Kutta Ode Solution', other:' source code' },
+{ tag: 'numeric_optimize_fun_class', title:'A Helper Class That Defines Functions Needed for Optimization', other:' syntax purpose objective_ad constraint_ad objective_fun objective_grad objective_hess constraint_fun constraint_jac constraint_hess example source code' },
+{ tag: 'numeric_optimize_fun_xam.py', title:'Example Using optimize_fun_class with Scipy Optimization', other:' reference problem trust_constr source code' },
+{ tag: 'numeric_seirs_class', title:'A Susceptible Exposed Infectious Recovered Model', other:' syntax set_t_all set_ode_p set_initial example source code' },
+{ tag: 'numeric_seirs_fit_xam.py', title:'Example Fitting an SEIRS Model', other:' source code' },
 { tag: 'library', title:'The Cppad Py Libraries', other:'' },
 { tag: 'py_lib', title:'The Python Library', other:'' },
 { tag: 'py_fun', title:'Cppad Py AD Functions', other:'' },
@@ -126,7 +132,7 @@ Keyword =
 { tag: 'error_message_xam.cpp', title:'C++: Cppad Py Exception Handling: Example and Test', other:'' },
 { tag: 'error_message_xam.py', title:'Python: Cppad Py Exception Handling: Example and Test', other:'' },
 { tag: 'more_cpp', title:'Steps To Add More C++ Functions', other:' purpose include file independent new_dynamic documentation example implementation testing' },
-{ tag: 'whats_new_2020', title:'CppAD Py Changes During 2020', other:' previous years 05-05 05-04 04-28 04-27 04-26 04-25 04-24 04-23 04-22 04-20 04-19 04-18 04-13 04-12 04-10' },
+{ tag: 'whats_new_2020', title:'CppAD Py Changes During 2020', other:' previous years 05-06 05-05 05-04 04-28 04-27 04-26 04-25 04-24 04-23 04-22 04-20 04-19 04-18 04-13 04-12 04-10' },
 { tag: 'whats_new_2018', title:'Cppad Py Changes During 2018', other:' 11-10 11-09 11-07 11-05 08-13 07-31 07-26 07-19 07-15 07-14 07-13 07-12 07-10 07-08 07-07 07-03' }
 ]
 
